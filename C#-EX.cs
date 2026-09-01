0d90d6890be42d31ca24706330e3035d0daf6f28
Francis Earl Montes Fernande <francishirofernandez@gmail.com>
2026-09-01 23:20:37 +0800

    modified:   C#-CHEAT SHEET.cs 	modified:   C#-EX.cs

--- C#-EX.cs
+++ C#-EX.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
         }   
 
 
