627ac7cb163a348348da82a8721b5d8e5680bd68
Francis Earl Montes Fernande <francishirofernandez@gmail.com>
2026-09-03 12:17:38 +0800

    modified:   C#-EX.cs

--- C#-EX.cs
+++ C#-EX.cs
@@ -6,19 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int day = 1;
-            switch (day)
-                {
-                    case 1:
-                        Console.WriteLine("Monday");
-                        break;
-                    case 2:
-                     Console.WriteLine("Tuesday");
-                        break;
-                    default:
-                        Console.WriteLine("Not a listed day");
-                        break;
-}
+            string firstName = "John";
+            string lastName = "Doe";
+            string name2 = String.Concat(firstName, lastName);
+            Console.WriteLine(name2);
         }   
 
 
