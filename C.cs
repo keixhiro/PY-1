1f34bcf5ba38bd6a44527cfcd54d0fa27dd2a011
Francis Earl Montes Fernande <francishirofernandez@gmail.com>
2026-09-17 20:27:06 +0800

    deleted:    C#-EX.cs 	new file:   C.cs

---
+++ C.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number: ");
+            String stringg = (Console.ReadLine());
+            lengthh = Convert.ToInt32(stringg);
+            int length = lengthh.Length;
+            int reverse = "";
+            reversee = Convert.ToString(reverse);
+
+            for(int i = length; i >= 1; i--)
+            {
+                int reverse += lengthh[i];
+            }
+            if reversee == lengthh
+            {
+                Console.WriteLine("Palindrome indeed");
+            }
+            
+        }
+    }
+}
+
