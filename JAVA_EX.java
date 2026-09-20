a9780ca4050446c11413a425b2d126682ba546fa
Francis Earl Montes Fernande <francishirofernandez@gmail.com>
2026-09-20 23:02:55 +0800

    modified:   JAVA_EX.java

--- JAVA_EX.java
+++ JAVA_EX.java
@@ -1,28 +1,30 @@
-package bscs_2_1n;
+package BSCS;
+
+public class Employee {
     
-    public class Employee {
+    static String companyName = "Tech Solutions Inc.";
+    String employeeName;
     
-        static String companyName = "Tech Solutions Inc.";
-        String employeeName;
-    
-        public void showDetails() {
-            double salary = 25000.00;
-            System.out.println("Company Name: " + companyName);
-            System.out.println("Employee Name: " + employeeName);
-            System.out.println("Salary: " + salary);
-            System.out.println("-----------------------------");
-        }
-    
-        public static void main(String[] args) {
-            Employee emp1 = new Employee();
-            Employee emp2 = new Employee();
-    
-            emp1.employeeName = "Francis Earl";
-            emp2.employeeName = "Beatrix Zoe";
-            emp1.showDetails();
-            emp2.showDetails();
-        }
+    public void showDetails() {
+        double salary = 25000.00;
+        System.out.println("Company Name: " + companyName);
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Salary: " + salary);
+        System.out.println("-----------------------------");
     }
+    
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+
+        emp1.employeeName = "Francis Earl";
+        emp2.employeeName = "Beatrix Zoe";
+        emp1.showDetails();
+        emp2.showDetails();
+    }
+}
+
+package BSCS;
 
 class Student {
     String name;
